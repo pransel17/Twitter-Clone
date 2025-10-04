688fd9a9649594c0376fe2aa2fa90af320043054
Rosalie Sarayan <lorrainesarayan@gmail.com>
2025-10-04 23:38:13 +0800

    partial backend done

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -5,18 +5,22 @@ export const getNotifications = async (req,res) => {
         const userId = req.user._id
         const notifications = await Notification.find({ to:userId}).populate({
             path: "from",
-            select: "username profileImg"
+            select: "Username ProfileImage" // only like take these from the array
         })
         await Notification.updateMany({to:userId}, {read:true})
         res.status(200).json(notifications)
-
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"})
     }
-
 }
 
 
 export const deleteNotifications = async (req,res) => {
-    
-}
+    try{
+        const userId = req.user._id
+        await Notification.deleteMany({ to: userId})
+        res.status(200).json({message: "Notifications deleted successfully"})
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}   
