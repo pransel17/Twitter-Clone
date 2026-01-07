6bdbbf4c1cc264408d2c71045c2d72d90eb99dab
Francelle Sarayan <lorrainesarayan@gmail.com>
2026-01-07 22:34:24 +0800

    sidebar idk

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
 import LoginPage from './pages/auth/login/LoginPage';
+import Sidebar from "../src/components/common/Sidebar.jsx"
 
 // app.jsx is the furniture, nandito yung mga routes for each pages ng app/web 
 // same routing like sa backend 
@@ -9,6 +10,7 @@ import LoginPage from './pages/auth/login/LoginPage';
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/signup' element={<SignUpPage />} />
